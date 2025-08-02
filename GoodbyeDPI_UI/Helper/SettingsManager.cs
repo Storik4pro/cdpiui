076dfefe309059611693b9475fe9c3a6e14f5c6a
Storik4pro <serstor15@gmail.com>
2025-08-02 15:07:00 +0300

    Add new overloads for GetValue and SetValue functions

--- GoodbyeDPI_UI/Helper/SettingsManager.cs
+++ GoodbyeDPI_UI/Helper/SettingsManager.cs
@@ -105,6 +105,46 @@ namespace GoodbyeDPI_UI.Helper
 
             throw new Exception($"Type mismatch or unsupported type for setting '{key}' in group '{group}'.");
         }
+        public T GetValue<T>(IEnumerable<string> groupPath, string key)
+        {
+            XElement current = _xDocument.Root;
+
+            foreach (var grp in groupPath)
+            {
+                current = current
+                    .Elements("Group")
+                    .FirstOrDefault(g => (string)g.Attribute("Name") == grp);
+                if (current == null)
+                {
+                    var defaultValue = GetDefaultValue<T>();
+                    SetValue(groupPath, key, defaultValue);
+                    Debug.WriteLine($"Group path '{string.Join("/", groupPath)}' not found.");
+                    return defaultValue;
+                }
+            }
+
+            var settingElement = current
+                .Elements("Setting")
+                .FirstOrDefault(s => (string)s.Attribute("Key") == key);
+
+            if (settingElement == null)
+            {
+                var defaultValue = GetDefaultValue<T>();
+                SetValue(groupPath, key, defaultValue);
+                Debug.WriteLine($"Setting '{key}' in group path '{string.Join("/", groupPath)}' not found.");
+                return defaultValue;
+            }
+
+            string value = (string)settingElement.Attribute("Value");
+            string type = (string)settingElement.Attribute("Type");
+
+            if (typeof(T) == typeof(int) && type == "int" && int.TryParse(value, out var iv)) return (T)(object)iv;
+            if (typeof(T) == typeof(double) && type == "double" && double.TryParse(value, out var dv)) return (T)(object)dv;
+            if (typeof(T) == typeof(bool) && type == "bool" && bool.TryParse(value, out var bv)) return (T)(object)bv;
+            if (typeof(T) == typeof(string) && type == "string") return (T)(object)value;
+
+            throw new Exception($"Type mismatch or unsupported type for setting '{key}' in group path '{string.Join("/", groupPath)}'.");
+        }
 
         public void SetValue<T>(string group, string key, T value)
         {
@@ -158,6 +198,68 @@ namespace GoodbyeDPI_UI.Helper
             else
             {
                 groupElement.Add(new XElement("Setting",
+                    new XAttribute("Key", key),
+                    new XAttribute("Type", type),
+                    new XAttribute("Value", valueString)));
+            }
+
+            _xDocument.Save(_filePath);
+        }
+        public void SetValue<T>(IEnumerable<string> groupPath, string key, T value)
+        {
+            string type;
+            string valueString;
+
+            switch (value)
+            {
+                case int i:
+                    type = "int";
+                    valueString = i.ToString();
+                    break;
+                case double d:
+                    type = "double";
+                    valueString = d.ToString();
+                    break;
+                case bool b:
+                    type = "bool";
+                    valueString = b.ToString().ToLower();
+                    break;
+                case string s:
+                    type = "string";
+                    valueString = s;
+                    break;
+                default:
+                    throw new Exception($"Unsupported value type: {typeof(T)}. Value is {value}");
+            }
+
+            XElement current = _xDocument.Root;
+            foreach (var grp in groupPath)
+            {
+                var next = current
+                    .Elements("Group")
+                    .FirstOrDefault(g => (string)g.Attribute("Name") == grp);
+
+                if (next == null)
+                {
+                    next = new XElement("Group", new XAttribute("Name", grp));
+                    current.Add(next);
+                }
+
+                current = next;
+            }
+
+            var setting = current
+                .Elements("Setting")
+                .FirstOrDefault(s => (string)s.Attribute("Key") == key);
+
+            if (setting != null)
+            {
+                setting.SetAttributeValue("Type", type);
+                setting.SetAttributeValue("Value", valueString);
+            }
+            else
+            {
+                current.Add(new XElement("Setting",
                     new XAttribute("Key", key),
                     new XAttribute("Type", type),
                     new XAttribute("Value", valueString)));
