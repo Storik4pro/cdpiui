3a49e3ee1a6e90c9cfcc9c15e3bd31f713293fb6
Storik4 <serstor15@gmail.com>
2025-08-07 12:17:05 +0300

    Add new dialog

---
+++ GoodbyeDPI_UI/Controls/Dialogs/ViewApplyArgsContentDialog.xaml.cs
@@ -0,0 +1,46 @@
+using GoodbyeDPI_UI.Helper;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace GoodbyeDPI_UI.Controls.Dialogs
+{
+    public sealed partial class ViewApplyArgsContentDialog : ContentDialog
+    {
+        public string DialogTitle { get; set; }
+        public string MessageToShow { get; set; }
+        public List<string> Args { get; set; }
+        public FontFamily ArgsFontFamily { get; set; }
+        public double ArgsFontSize { get; set; }
+
+        public ViewApplyArgsContentDialog()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+            this.Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style;
+
+            ArgsFontFamily = new FontFamily(SettingsManager.Instance.GetValue<string>("PSEUDOCONSOLE", "fontFamily"));
+            ArgsFontSize = SettingsManager.Instance.GetValue<double>("PSEUDOCONSOLE", "fontSize");
+        }
+
+        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+
+        }
+    }
+}
