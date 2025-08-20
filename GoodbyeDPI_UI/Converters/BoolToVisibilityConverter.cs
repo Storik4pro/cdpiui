e9c63eae947b08e5ff2442b577ff1804058fb27e
Storik4pro <serstor15@gmail.com>
2025-08-20 17:47:09 +0300

    Add converter

---
+++ GoodbyeDPI_UI/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodbyeDPI_UI.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool b && b)
+                return Visibility.Visible;
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
