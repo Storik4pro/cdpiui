0e0dd0fd6ce2d274af942b80355ba4e59eb72ec2
Storik4pro <serstor15@gmail.com>
2025-08-20 17:52:04 +0300

    Add new state fields

--- GoodbyeDPI_UI/Helper/StateHelper.cs
+++ GoodbyeDPI_UI/Helper/StateHelper.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using GoodbyeDPI_UI.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using GoodbyeDPI_UI.ViewModels;
 
 namespace GoodbyeDPI_UI.Helper
 {
@@ -39,6 +41,12 @@ namespace GoodbyeDPI_UI.Helper
 
         public readonly Dictionary<string, string> FileTypes = new();
 
+        // Local
+
+        public const string LocalUserItemsId = "LocalUserStorage";
+        public const string LocalUserItemSiteListsFolder = "List";
+        public const string LocalUserItemBinsFolder = "Bin";
+
         // Components
 
         public ComponentSettings goodbyedpiSettings = new("goodbyedpi");
@@ -46,13 +54,32 @@ namespace GoodbyeDPI_UI.Helper
         public ComponentSettings byedpiSettings = new("byedpi");
         public ComponentSettings spoofdpiSettings = new("spoofdpi");
 
+        public Dictionary<string, string> ComponentIdPairs = new();
+
         public bool isCheckedComponentsUpdateComplete = false;
         public string lastComponentsUpdateError = "";
+
+        public string Version;
 
         private StateHelper()
         {
             FileTypes.Add("archive", ".zip");
+
+            string exePath = Assembly.GetEntryAssembly()?.Location
+                 ?? Assembly.GetExecutingAssembly()?.Location
+                 ?? Process.GetCurrentProcess().MainModule?.FileName;
+            var fvi = FileVersionInfo.GetVersionInfo(exePath);
+            Version = fvi.FileVersion;
+            string productVersion = fvi.ProductVersion; 
+            Console.WriteLine($"FileVersion: {Version}, ProductVersion: {productVersion}");
+
+            ComponentIdPairs.Add("ASGKOI001", "GoodCheck");
+            ComponentIdPairs.Add("CSZTBN012", "Zapret");
         }
 
+        public string FindKeyByValue(string value)
+        {
+            return ComponentIdPairs.FirstOrDefault(kvp => kvp.Value == value).Key;
+        }
     }
 }
