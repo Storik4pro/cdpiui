c3a5835e8c33ad2341d347dd223effa7b0ece5c2
Storik4pro <serstor15@gmail.com>
2025-08-27 15:44:09 +0300

    Enhance SitelistButton with command binding features
    Updated SitelistButton.xaml to add a new column for PackName and set its text alignment.
    Modified SitelistButton.xaml.cs to include new using directives and introduced dependency properties for ClickCommand, ClickCommandParameter, Directory, and PackId.
    Updated Button_Click method to support command execution with parameters.

--- GoodbyeDPI_UI/Controls/SitelistButton.xaml.cs
+++ GoodbyeDPI_UI/Controls/SitelistButton.xaml.cs
@@ -1,10 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -12,6 +5,14 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -20,10 +21,36 @@ namespace GoodbyeDPI_UI;
 
 public sealed partial class SitelistButton : UserControl
 {
+    public static readonly DependencyProperty ClickCommandProperty =
+            DependencyProperty.Register(
+                nameof(ClickCommand),
+                typeof(ICommand),
+                typeof(SitelistButton),
+                new PropertyMetadata(null)
+            );
+
+    public ICommand ClickCommand
+    {
+        get => (ICommand)GetValue(ClickCommandProperty);
+        set => SetValue(ClickCommandProperty, value);
+    }
+
+    public static readonly DependencyProperty ClickCommandParameterProperty =
+        DependencyProperty.Register(
+            nameof(ClickCommandParameter),
+            typeof(object),
+            typeof(SitelistButton),
+            new PropertyMetadata(null)
+        );
+
+    public object ClickCommandParameter
+    {
+        get => GetValue(ClickCommandParameterProperty);
+        set => SetValue(ClickCommandParameterProperty, value);
+    }
+
     public Action Click;
 
-    public string Directory;
-    public string PackId;
     public SitelistButton()
     {
         InitializeComponent();
@@ -50,9 +77,36 @@ public sealed partial class SitelistButton : UserControl
         DependencyProperty.Register(
             nameof(PackName), typeof(string), typeof(SitelistButton), new PropertyMetadata(string.Empty)
         );
+    public string Directory
+    {
+        get { return (string)GetValue(DirectoryProperty); }
+        set { SetValue(DirectoryProperty, value); }
+    }
+
+    public static readonly DependencyProperty DirectoryProperty =
+        DependencyProperty.Register(
+            nameof(Directory), typeof(string), typeof(SitelistButton), new PropertyMetadata(string.Empty)
+        );
+
+    public string PackId
+    {
+        get { return (string)GetValue(PackIdProperty); }
+        set { SetValue(PackIdProperty, value); }
+    }
+
+    public static readonly DependencyProperty PackIdProperty =
+        DependencyProperty.Register(
+            nameof(PackId), typeof(string), typeof(SitelistButton), new PropertyMetadata(string.Empty)
+        );
 
     private void Button_Click(object sender, RoutedEventArgs e)
     {
         Click?.Invoke();
+        ClickCommandParameter = Tuple.Create(Directory, PackId);
+        if (ClickCommand != null && ClickCommand.CanExecute(ClickCommandParameter))
+        {
+            ClickCommand.Execute(ClickCommandParameter);
+            return;
+        }
     }
 }
