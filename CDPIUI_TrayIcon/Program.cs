baa95ac95cadbd2b22d01421865b6f64651298c0
Storik4pro <serstor15@gmail.com>
2025-09-11 18:48:06 +0300

    Fix bug

--- CDPIUI_TrayIcon/Program.cs
+++ CDPIUI_TrayIcon/Program.cs
@@ -7,21 +7,8 @@ using Application = System.Windows.Forms.Application;
 
 class Programm
 {
-    private const string MutexName = "Global\\AppTrayIconMutex_3f6a1b9d-8c2a-4f3b-9f2a-0f1a2b3c4d5e";
-    private static bool created;
-
     static void Main(string[] args)
     {
-        using (var mutex = new Mutex(true, MutexName, out created))
-        {
-            if (!created)
-            {
-                MessageBox.Show("Tray app is already running.");
-                return;
-            }
-
-            
-        }
         PipeServer.Instance.Init();
         PipeServer.Instance.Start();
 
