edcbbc60687d336e10bdf1a2741fdc387b002e4d
Storik4pro <serstor15@gmail.com>
2025-09-11 17:50:38 +0300

    Add logging to file

--- GoodbyeDPI_UI/Helper/Logger.cs
+++ GoodbyeDPI_UI/Helper/Logger.cs
@@ -3,10 +3,13 @@ using Microsoft.UI.Xaml;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using static GoodbyeDPI_UI.Helper.ErrorsHelper;
+using Exception = System.Exception;
 
 namespace GoodbyeDPI_UI.Helper
 {
@@ -28,6 +31,8 @@ namespace GoodbyeDPI_UI.Helper
             }
         }
 
+        private string m_exePath;
+
         public Logger()
         {
 
@@ -35,11 +40,61 @@ namespace GoodbyeDPI_UI.Helper
 
         private static readonly object _logLock = new object();
 
+        private void LogWrite(string fileName, string logMessage)
+        {
+            m_exePath = StateHelper.GetDataDirectory();
+            
+            try
+            {
+                string logFileDir = Path.Combine(m_exePath, "Logs");
+                string logFilePath = Path.Combine(logFileDir, $"{fileName}.log");
+
+                if (!Directory.Exists(logFileDir)) Directory.CreateDirectory(logFileDir);
+                if (!File.Exists(logFilePath))
+                {
+                    File.Create(logFilePath);
+                    File.WriteAllText(logFilePath, "Logger is ready-to-work" + Environment.NewLine);
+                }
+
+                using StreamWriter w = File.AppendText(logFilePath);
+                Log(logMessage, w);
+            }
+            catch 
+            {
+            }
+        }
+
+        private static void Log(string logMessage, TextWriter txtWriter)
+        {
+            try
+            {
+                txtWriter.WriteLine("[{0} {1}] {2}", DateTime.Now.ToLongTimeString(), 
+                    DateTime.Now.ToShortDateString(), logMessage);
+            }
+            catch 
+            {
+            }
+        }
+
         public void CreateLog(string message, string severity, string sender) 
         {
             lock (_logLock)
             {
-                Debug.WriteLine($"[{sender}] [{severity}] {message}");
+                string logMessage = $"[{sender}] [{severity}] {message}";
+                bool write = true;
+                try
+                {
+                    string _sev = SettingsManager.Instance.GetValue<string>("DEBUG", "logLevel");
+                    if (_sev == "DEBG") write = true;
+                    else if (_sev == "INFO" && severity != "DEBG") write = true;
+                    else if (_sev == "WARN" && severity != "DEBG" && severity != "INFO") write = true;
+                    else if (_sev == "CRIT" && severity != "DEBG" && severity != "INFO" && severity != "WARN") write = true;
+                    else write = false;
+                }
+                catch { }
+
+                if (write) 
+                    LogWrite(sender, logMessage);
             }
         }
 
