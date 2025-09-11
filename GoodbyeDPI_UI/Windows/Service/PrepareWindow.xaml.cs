5ac83e9a1aacd45e9bb435559f6e498842c2516d
Storik4pro <serstor15@gmail.com>
2025-09-11 17:55:49 +0300

    Add getting ready window

---
+++ GoodbyeDPI_UI/Windows/Service/PrepareWindow.xaml.cs
@@ -0,0 +1,138 @@
+using GoodbyeDPI_UI.Helper;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using WinRT.Interop;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace GoodbyeDPI_UI
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PrepareWindow : Window
+    {
+        private const int WM_GETMINMAXINFO = 0x0024;
+        private IntPtr _hwnd;
+        private WindowProc _newWndProc;
+        private IntPtr _oldWndProc;
+
+        public PrepareWindow()
+        {
+            InitializeComponent();
+            InitializeWindow();
+
+            GetAppWindowAndPresenter();
+            _apw.IsShownInSwitchers = false;
+            _presenter.SetBorderAndTitleBar(true, false);
+
+            var appWindowPresenter = this.AppWindow.Presenter as OverlappedPresenter;
+            appWindowPresenter.IsResizable = false;
+            appWindowPresenter.IsMaximizable = false;
+            appWindowPresenter.IsMinimizable = false;
+
+            WindowHelper.SetWindowSize(this, 400, 150);
+
+            ((App)Application.Current).OpenWindows.Add(this);
+
+            if (this.Content is FrameworkElement rootElement)
+            {
+                rootElement.RequestedTheme = ((App)Application.Current).CurrentTheme;
+            }
+
+            ExtendsContentIntoTitleBar = true;
+
+            SetTitleBar(WindowMoveAera);
+
+            this.Closed += CriticalErrorHandlerWindow_Closed;
+
+            PipeClient.Instance.Connected += PipeConnected;
+        }
+        private void PipeConnected()
+        {
+            this.Close();
+        }
+        private void CriticalErrorHandlerWindow_Closed(object sender, WindowEventArgs args)
+        {
+            if (!PipeClient.Instance.IsConnected)
+                args.Handled = true;
+        }
+
+        ~PrepareWindow()
+        {
+        }
+
+        public void GetAppWindowAndPresenter()
+        {
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            WindowId myWndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            _apw = AppWindow.GetFromWindowId(myWndId);
+            _presenter = _apw.Presenter as OverlappedPresenter;
+        }
+        private AppWindow _apw;
+        private OverlappedPresenter _presenter;
+
+        private void InitializeWindow()
+        {
+            _hwnd = WindowNative.GetWindowHandle(this);
+            _newWndProc = new WindowProc(NewWindowProc);
+            _oldWndProc = SetWindowLongPtr(_hwnd, GWLP_WNDPROC, Marshal.GetFunctionPointerForDelegate(_newWndProc));
+        }
+        private delegate IntPtr WindowProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+        private IntPtr NewWindowProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam)
+        {
+            if (msg == WM_GETMINMAXINFO)
+            {
+                MINMAXINFO minMaxInfo = Marshal.PtrToStructure<MINMAXINFO>(lParam);
+                minMaxInfo.ptMinTrackSize.x = 0;
+                minMaxInfo.ptMinTrackSize.y = 0;
+                Marshal.StructureToPtr(minMaxInfo, lParam, true);
+            }
+            return CallWindowProc(_oldWndProc, hWnd, msg, wParam, lParam);
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MINMAXINFO
+        {
+            public POINT ptReserved;
+            public POINT ptMaxSize;
+            public POINT ptMaxPosition;
+            public POINT ptMinTrackSize;
+            public POINT ptMaxTrackSize;
+        }
+
+        private const int GWLP_WNDPROC = -4;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr SetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr CallWindowProc(IntPtr lpPrevWndFunc, IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+    }
+}
