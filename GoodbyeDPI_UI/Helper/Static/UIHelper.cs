a2adfa8f8fd1454206c684323718c0f7b62cef63
Storik4pro <serstor15@gmail.com>
2025-09-21 17:25:33 +0300

    Add new helpers

--- GoodbyeDPI_UI/Helper/Static/UIHelper.cs
+++ GoodbyeDPI_UI/Helper/Static/UIHelper.cs
@@ -1,13 +1,19 @@
-﻿using Microsoft.UI;
+﻿using CDPI_UI.Helper.Items;
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Automation.Peers;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
+using Padding = System.Windows.Forms.Padding;
 using Windows.UI;
+using Microsoft.UI.Xaml.Media.Imaging;
 
 namespace CDPI_UI.Helper.Static
 {
@@ -115,6 +121,267 @@ namespace CDPI_UI.Helper.Static
 
             if (frame.CanGoBack)
                 frame.GoBack();
+        }
+
+        public enum SettingTileContentType
+        {
+            OnlyViewButton,
+            EditViewButtons,
+            ToggleSwitch,
+            SiteListToggle,
+            FullButton,
+            OnlyTextContent,
+            ComboBoxSelector
+        }
+        public class ComboBoxModel
+        {
+            public string DisplayName { get; set; }
+            public string Id { get; set; }
+        }
+
+        public class SettingTileContentDefinition
+        {
+            public SettingTileContentType ContentType { get; set; }
+
+            public string ActionGlyph { get; set; }
+
+            public string Text { get; set; }
+
+            public string VariableName { get; set; }
+            public bool InitialToggleState { get; set; }
+            public string PackId { get; set; }
+            public string FileName { get; set; }
+
+            public string EditFilePath { get; set; }
+            public List<string> ViewParams { get; set; }
+            public List<string> PrettyViewParams { get; set; }
+
+            public string ClickId { get; set; }
+
+            public List<ComboBoxModel> ComboBoxItems { get; set; }
+            public string SelectedComboBoxItemId { get; set; }
+        }
+
+        public class SettingsTileItem
+        {
+            public string Title { get; set; }
+            public bool ShowTopRectangle { get; set; }
+            public IList<SettingTileContentDefinition> Contents { get; } = new List<SettingTileContentDefinition>();
+        }
+
+        public partial class SettingsTile : INotifyPropertyChanged
+        {
+            public string IconGlyph { get; set; } = "";
+            public string Title { get; set; } = "";
+            public string Description { get; set; } = "";
+            public ObservableCollection<SettingsTileItem> Items { get; } = new ObservableCollection<SettingsTileItem>();
+
+            public event PropertyChangedEventHandler PropertyChanged;
+        }
+
+        public enum ActionIds
+        {
+            None,
+            ViewButtonClicked,
+            EditButtonClicked,
+            SwitchToggled,
+            FullButtonElementClicked,
+            ComboBoxSelectionChanged
+        }
+
+        public enum  TileType
+        {
+            Basic,
+            QuickWidget
+        }
+
+        private static Thickness _Padding = new Thickness(20, 10, 20, 10);
+
+        static public UIElement CreateSettingTile(
+            SettingsTile preset, 
+            Action<ActionIds, List<string>, SettingTileContentDefinition> executeAction, 
+            TileType tileType = TileType.Basic,
+            Thickness? padding = null)
+        {
+            if (padding == null) padding = _Padding;
+            var rootStack = new StackPanel();
+
+            foreach (var list in preset.Items)
+            {
+                var element = new SettingTileControlElement
+                {
+                    Title = list.Title,
+                    ShowTopRectangle = list.ShowTopRectangle,
+                    CardPadding = (Thickness)padding
+                };
+
+                var contentPanel = new StackPanel { Orientation = Orientation.Horizontal, Spacing = 8 };
+
+                foreach (var def in list.Contents)
+                {
+                    switch (def.ContentType)
+                    {
+                        case SettingTileContentType.OnlyViewButton:
+                            var _viewBtn = new Button { Padding = new Thickness(6) };
+                            _viewBtn.Content = new FontIcon { Glyph = "\uE890", FontSize = 16 };
+                            _viewBtn.Click += (s, e) =>
+                            {
+                                executeAction?.Invoke(ActionIds.ViewButtonClicked, [list.Title], def);
+                            };
+
+                            contentPanel.Children.Add(_viewBtn);
+                            break;
+
+                        case SettingTileContentType.EditViewButtons:
+                            var editBtn = new Button { Padding = new Thickness(6) };
+                            editBtn.Content = new FontIcon { Glyph = "\uE70F", FontSize = 16 };
+                            editBtn.Click += (s, e) =>
+                            {
+                                executeAction?.Invoke(ActionIds.EditButtonClicked, null, def);
+                            };
+
+                            var viewBtn = new Button { Padding = new Thickness(6) };
+                            viewBtn.Content = new FontIcon { Glyph = "\uE890", FontSize = 16 };
+                            viewBtn.Click += (s, e) =>
+                            {
+                                executeAction?.Invoke(ActionIds.ViewButtonClicked, [list.Title], def);
+                            };
+
+                            contentPanel.Children.Add(editBtn);
+                            contentPanel.Children.Add(viewBtn);
+                            break;
+
+                        case SettingTileContentType.ToggleSwitch:
+                            var toggle = new ToggleSwitch
+                            {
+                                IsOn = def.InitialToggleState
+                            };
+                            toggle.Toggled += (s, e) =>
+                            {
+                                executeAction?.Invoke(ActionIds.SwitchToggled, [toggle.IsOn.ToString()], def);
+                                
+                            };
+                            contentPanel.Children.Add(toggle);
+                            break;
+                        case SettingTileContentType.FullButton:
+                            element.ActionIconGlyph = "\uE8A7";
+                            element.IsClickEnabled = true;
+
+                            element.Click += () =>
+                            {
+                                executeAction?.Invoke(ActionIds.FullButtonElementClicked, null, def);
+                            };
+                            break;
+                        case SettingTileContentType.OnlyTextContent:
+                            var textBlock = new TextBlock
+                            {
+                                Text = def.Text,
+                                TextWrapping = TextWrapping.WrapWholeWords
+                            };
+                            contentPanel.Children.Add(textBlock);
+                            break;
+                        case SettingTileContentType.ComboBoxSelector:
+                            var comboBox = new ComboBox
+                            {
+                                ItemsSource = def.ComboBoxItems,
+                                DisplayMemberPath = "DisplayName",
+                                SelectedValuePath = "Id",
+                                SelectedItem = def.ComboBoxItems.FirstOrDefault(x => x.Id == def.SelectedComboBoxItemId)
+                            };
+                            comboBox.SelectionChanged += (s, e) =>
+                            {
+                                executeAction?.Invoke(ActionIds.ComboBoxSelectionChanged, [((ComboBoxModel)comboBox.SelectedItem).Id], def);
+                            };
+                            contentPanel.Children.Add(comboBox);
+                            break;
+                    }
+                }
+
+                element.InnerContent = contentPanel;
+                rootStack.Children.Add(element);
+            }
+
+            
+
+            if (tileType == TileType.Basic)
+            {
+                var tile = new SettingTile
+                {
+                    IconGlyph = preset.IconGlyph,
+                    Title = preset.Title,
+                    Description = preset.Description,
+                    InnerContent = rootStack
+                };
+                return tile;
+            }
+            else if (tileType == TileType.QuickWidget)
+            {
+                var tile = new QuickSettingWidget
+                {
+                    IconGlyph = preset.IconGlyph,
+                    Title = preset.Title,
+                    Description = preset.Description,
+                    InnerContent = rootStack
+                };
+                return tile;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static StoreItemLargeButton CreateLargeButton(string storeId, string imageSource, string price, string title, string backgroundColor, Action<StoreItemLargeButton> action, string developer = "")
+        {
+            StoreItemLargeButton storeItemLargeButton;
+
+            string eImageSource = StoreHelper.Instance.ExecuteScript(imageSource);
+            BitmapImage image = new BitmapImage(new Uri(eImageSource));
+
+            Windows.UI.Color color = HexToColorConverter(backgroundColor);
+
+            storeItemLargeButton = new StoreItemLargeButton
+            {
+                StoreId = storeId,
+                CardTitle = title,
+                CardDeveloper = developer,
+                CardImageSource = image,
+                CardPrice = price,
+                CardBackgroundColor = color,
+            };
+
+            storeItemLargeButton.Click += action;
+
+            return storeItemLargeButton;
+        }
+
+        public static StoreItemSmallButton CreateSmallButton(string storeId, string imageSource, string price, string title, string developer, string backgroundColor, Action<StoreItemSmallButton> action)
+        {
+            StoreItemSmallButton storeItemSmallButton;
+
+            string eImageSource = StoreHelper.Instance.ExecuteScript(imageSource);
+            BitmapImage image = new BitmapImage(new Uri(eImageSource));
+
+            SolidColorBrush solidColorBrush = UIHelper.HexToSolidColorBrushConverter(backgroundColor);
+
+            storeItemSmallButton = new StoreItemSmallButton
+            {
+                StoreId = storeId,
+                CardTitle = title,
+                CardImageSource = image,
+                CardPrice = price,
+                CardDeveloper = developer,
+                CardBackgroundBrush = solidColorBrush,
+            };
+
+            storeItemSmallButton.Click += action;
+
+            return storeItemSmallButton;
+        }
+
+        public static string GetWindowName(string windowName)
+        {
+            return $"{windowName} — CDPI UI";
         }
 
     }
