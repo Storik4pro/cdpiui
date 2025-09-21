5adc84e56aabfc046e0ab32d24e8f4d12dbe7d67
Storik4pro <serstor15@gmail.com>
2025-09-21 17:14:27 +0300

    Integrate toast notifications and command-line handling
    Updated the application to use Microsoft.Toolkit.Uwp.Notifications for toast notifications. Modified the `Main` method to handle notification activation and clear history. Added command-line argument support for showing the UI and starting minimized in the system tray. Introduced `ToastNotificationManagerCompat_OnActivated` to manage user input from notifications.

--- CDPIUI_TrayIcon/Program.cs
+++ CDPIUI_TrayIcon/Program.cs
@@ -1,22 +1,60 @@
 ﻿using CDPIUI_TrayIcon.Helper;
-using System.Drawing;
-using System.Runtime.InteropServices;
-using System.Windows.Forms;
-using static System.Net.Mime.MediaTypeNames;
+using Microsoft.Toolkit.Uwp.Notifications;
+using Windows.Foundation.Collections;
 using Application = System.Windows.Forms.Application;
 
 class Programm
 {
+    [STAThread]
     static void Main(string[] args)
     {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+
+        ToastNotificationManagerCompat.OnActivated += ToastNotificationManagerCompat_OnActivated;
+
+        ToastNotificationManagerCompat.History.Clear();
+
         PipeServer.Instance.Init();
         PipeServer.Instance.Start();
 
         _ = TrayIconHelper.Instance;
-        
 
+        if (ToastNotificationManagerCompat.WasCurrentProcessToastActivated())
+        {
+            Application.Run();
+            return;
+        }
+
+        if (args.Contains("--show-ui"))
+        {
+            RunHelper.RunAsDesktopUser(Path.Combine(Utils.GetDataDirectory(), "CDPIUI.exe"), string.Empty);
+        }
+
+        if (args.Contains("--autorun"))
+        {
+            _ = ProcessManager.Instance.StartProcess();
+            TrayIconHelper.Instance.ShowMessage("CDPI UI", "Application is runned and minimized to tray now.\nClick or tap here to open main window", "SHOW_MAIN_WINDOW");
+        }
 
         Application.Run();
+        ToastNotificationManagerCompat.History.Clear();
     }
 
+    private static void ToastNotificationManagerCompat_OnActivated(ToastNotificationActivatedEventArgsCompat toastArgs)
+    {
+        try
+        {
+            var _toastArgs = ToastArguments.Parse(toastArgs.Argument);
+
+            if (_toastArgs.TryGetValue("action", out string action))
+            {
+                TrayIconHelper.Instance.HandleToastActionFromBackground(action);
+            }
+            else { }
+
+            ValueSet userInput = toastArgs.UserInput;
+        }
+        catch { }
+    }
 }
