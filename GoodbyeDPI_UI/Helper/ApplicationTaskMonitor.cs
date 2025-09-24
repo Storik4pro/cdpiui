fe7b1c2a243ebce5aa9811d739d042a00b7785d0
Storik4pro <serstor15@gmail.com>
2025-09-24 19:05:26 +0300

    Add ApplicationTaskMonitor

---
+++ GoodbyeDPI_UI/Helper/ApplicationTaskMonitor.cs
@@ -0,0 +1,48 @@
+﻿using CDPI_UI.Helper.CreateConfigUtil.GoodCheck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDPI_UI.Helper
+{
+    public class ApplicationTaskMonitor
+    {
+        private static ApplicationTaskMonitor _instance;
+        private static readonly object _lock = new object();
+
+        public static ApplicationTaskMonitor Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                        _instance = new ApplicationTaskMonitor();
+                    return _instance;
+                }
+            }
+        }
+
+        private ApplicationTaskMonitor()
+        {
+
+        }
+
+        public static bool IsComponentProcessRunned()
+        {
+            return ProcessManager.Instance.processState;
+        }
+
+        public static bool IsGoodCheckRunned()
+        {
+            return GoodCheckProcessHelper.Instance.IsRunned();
+        }
+
+        public static bool IsStoreWorking()
+        {
+            return StoreHelper.Instance.IsNowUpdatesChecked || !string.IsNullOrEmpty(StoreHelper.Instance.GetCurrentQueueOperationId());
+        }
+    }
+}
