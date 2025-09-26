d759a54bceea20b0266d1f5217a480b59a887367
Storik4pro <serstor15@gmail.com>
2025-09-26 16:02:03 +0300

    Add new helper

---
+++ GoodbyeDPI_UI/Helper/UrlOpenHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDPI_UI.Helper
+{
+    public static class UrlOpenHelper
+    {
+        public static async void LaunchReportUrl()
+        {
+            _ = await Windows.System.Launcher.LaunchUriAsync(new Uri("https://github.com/Storik4pro/cdpiui/issues"));
+        }
+        public static async void LaunchMainRepoUrl()
+        {
+            _ = await Windows.System.Launcher.LaunchUriAsync(new Uri("https://github.com/Storik4pro/cdpiui"));
+        }
+        public static async void LaunchWikiUrl()
+        {
+            _ = await Windows.System.Launcher.LaunchUriAsync(new Uri("https://storik4pro.github.io/cdpiui/"));
+        }
+    }
+}
