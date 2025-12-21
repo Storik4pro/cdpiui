2ffa3250b6ebafe7f07c05268e30686f86cf336a
Storik4pro <serstor15@gmail.com>
2025-12-21 18:06:17 +0300

    Add new control

---
+++ GoodbyeDPI_UI/Controls/Universal/HorizontalScrollContainer.xaml.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace CDPI_UI.Controls.Universal;
+
+public sealed partial class HorizontalScrollContainer : UserControl
+{
+    public HorizontalScrollContainer()
+    {
+        this.InitializeComponent();
+    }
+
+    public object Source
+    {
+        get => (object)GetValue(SourceProperty);
+        set => SetValue(SourceProperty, value);
+    }
+
+    public static readonly DependencyProperty SourceProperty =
+        DependencyProperty.Register(nameof(Source), typeof(object), typeof(HorizontalScrollContainer), new PropertyMetadata(null));
+
+    private void Scroller_ViewChanging(object sender, ScrollViewerViewChangingEventArgs e)
+    {
+        if (e.FinalView.HorizontalOffset < 1)
+        {
+            ScrollBackBtn.Visibility = Visibility.Collapsed;
+        }
+        else if (e.FinalView.HorizontalOffset > 1)
+        {
+            ScrollBackBtn.Visibility = Visibility.Visible;
+        }
+
+        if (e.FinalView.HorizontalOffset > scroller.ScrollableWidth - 1)
+        {
+            ScrollForwardBtn.Visibility = Visibility.Collapsed;
+        }
+        else if (e.FinalView.HorizontalOffset < scroller.ScrollableWidth - 1)
+        {
+            ScrollForwardBtn.Visibility = Visibility.Visible;
+        }
+    }
+
+    private void ScrollBackBtn_Click(object sender, RoutedEventArgs e)
+    {
+        scroller.ChangeView(scroller.HorizontalOffset - scroller.ViewportWidth, null, null);
+
+        // Manually focus to ScrollForwardBtn since this button disappears after scrolling to the end.
+        ScrollForwardBtn.Focus(FocusState.Programmatic);
+    }
+
+    private void ScrollForwardBtn_Click(object sender, RoutedEventArgs e)
+    {
+        scroller.ChangeView(scroller.HorizontalOffset + scroller.ViewportWidth, null, null);
+
+        // Manually focus to ScrollBackBtn since this button disappears after scrolling to the end.
+        ScrollBackBtn.Focus(FocusState.Programmatic);
+    }
+
+    private void Scroller_SizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        UpdateScrollButtonsVisibility();
+    }
+
+    private void UpdateScrollButtonsVisibility()
+    {
+        if (scroller.ScrollableWidth > 0)
+        {
+            ScrollForwardBtn.Visibility = Visibility.Visible;
+        }
+        else
+        {
+            ScrollForwardBtn.Visibility = Visibility.Collapsed;
+        }
+    }
+}
