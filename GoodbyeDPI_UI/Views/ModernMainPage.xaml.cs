b7a2df0c93e3dbb8827a92b5532185370424b09d
Storik4pro <serstor15@gmail.com>
2025-12-27 19:52:32 +0300

    Fix markup

--- GoodbyeDPI_UI/Views/ModernMainPage.xaml.cs
+++ GoodbyeDPI_UI/Views/ModernMainPage.xaml.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using CDPI_UI.Helper;
+using CDPI_UI.Helper.LScript;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -12,11 +7,18 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-using System.Diagnostics;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using CDPI_UI.Helper;
-using CDPI_UI.Helper.LScript;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.StartScreen;
+using WinRT.Interop;
 using WinUI3Localizer;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -41,6 +43,7 @@ public sealed partial class ModernMainPage : Page
 
     public double ElementWidth { get; set; } = 210;
     public double ElementHeight { get; set; } = 250;
+    public double Spacing { get; set; } = 0;
 
     private ILocalizer localizer = Localizer.Get();
     public ModernMainPage()
@@ -72,6 +75,7 @@ public sealed partial class ModernMainPage : Page
     {
         TileModels.Clear();
         List<DatabaseStoreItem> installedComponents = DatabaseHelper.Instance.GetItemsByType("component");
+        CalcWidth(SettingsManager.Instance.GetValue<int>("APPEARANCE", "mainGridColumnsCount"), installedComponents.Count);
         foreach (DatabaseStoreItem installedComponent in installedComponents)
         {
             TileModels.Add(new ComponentTileModel()
@@ -87,16 +91,32 @@ public sealed partial class ModernMainPage : Page
 
     private void ModernMainPage_Loaded(object sender, RoutedEventArgs e)
     {
-        CalcWidth();
         MainGridView.ItemsSource = TileModels;
         CreateTiles();
+        
     }
 
-    private void CalcWidth()
+    private void CalcWidth(int columns, int elCount)
     {
         double pageWidth = this.ActualWidth;
-        Debug.WriteLine(pageWidth);
-        ElementWidth = (pageWidth - 40 - 45) / 4;
+        
+        if (columns < 0)
+        {
+            if (elCount == 1)
+            {
+                columns = 1;
+            }
+            else if (elCount == 2)
+            {
+                columns = 2;
+            }
+            else
+            {
+                columns = 4;
+            }
+        }
+        ElementWidth = (pageWidth - 40 - (10 * (columns - 1))) / columns;
+        Spacing = columns != 1 ? 10 : 0;
     }
 
     private void AuditMarkup()
@@ -113,9 +133,16 @@ public sealed partial class ModernMainPage : Page
         }
     }
 
+    private async void ShowDialog(string message, string title)
+    {
+        var dlg = new MessageDialog(message, title);
+        InitializeWithWindow.Initialize(dlg, WindowNative.GetWindowHandle(await ((App)Application.Current).SafeCreateNewWindow<ModernMainWindow>()));
+        await dlg.ShowAsync();
+    }
+
     private void ApplicationSetupHelperButton_Click(object sender, RoutedEventArgs e)
     {
-
+        ShowDialog(localizer.GetLocalizedString("PreviewVersionDescription"), localizer.GetLocalizedString("PreviewVersion"));
     }
 
     private async void GetNewComponentsFromStoreButton_Click(object sender, RoutedEventArgs e)
