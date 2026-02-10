f2322ba0d394fc002f3bd480f67d28d795420bd5
Storik4pro <serstor15@gmail.com>
2026-02-10 19:24:52 +0300

    Add new model

---
+++ GoodbyeDPI_UI/ViewModels/BreadcrumbBarModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDPI_UI.ViewModels
+{
+    public class BreadcrumbBarModel
+    {
+        public string DisplayName { get; set; }
+        public Type Tag { get; set; }
+    }
+}
