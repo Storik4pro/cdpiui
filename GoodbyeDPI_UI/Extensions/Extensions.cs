5b7f4d2867037249a5198cb44e4511a57e520017
Storik4pro <serstor15@gmail.com>
2026-02-25 09:52:41 +0300

    Add new extensions

--- GoodbyeDPI_UI/Extensions/Extensions.cs
+++ GoodbyeDPI_UI/Extensions/Extensions.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI;
 
 namespace CDPI_UI.Extensions
 {
@@ -14,6 +15,17 @@ namespace CDPI_UI.Extensions
             List<T> sorted = collection.OrderBy(x => x).ToList();
             for (int i = 0; i < sorted.Count(); i++)
                 collection.Move(collection.IndexOf(sorted[i]), i);
+        }
+
+        public static string ColorToHex(this Color c) => $"#{c.R:X2}{c.G:X2}{c.B:X2}";
+
+        public static void AddRange<T>(this ICollection<T> target, IEnumerable<T> source)
+        {
+            ArgumentNullException.ThrowIfNull(target);
+            ArgumentNullException.ThrowIfNull(source);
+            foreach (var element in source)
+                if (!target.Contains(element))
+                    target.Add(element);
         }
     }
 }
