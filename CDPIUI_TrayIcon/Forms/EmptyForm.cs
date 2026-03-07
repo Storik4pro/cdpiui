d471b1c592853e2b9ab7189dcabe46285814f191
Storik4pro <serstor15@gmail.com>
2026-03-07 19:37:13 +0300

    Add new forms

---
+++ CDPIUI_TrayIcon/Forms/EmptyForm.cs
@@ -0,0 +1,423 @@
+﻿using CDPIUI_TrayIcon.Helper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using static CDPIUI_TrayIcon.Forms.TrayMenuForm;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CDPIUI_TrayIcon.Forms
+{
+    // Used for creating tray menu handler only.
+
+    public partial class EmptyForm : Form
+    {
+        private static readonly Guid IconDisplayGuid = new("D6AF8980-885B-453C-908C-DD79AC1F2AB2");
+
+        private TrayMenuForm? TrayMenuForm;
+
+        private readonly CancellationTokenSource? CancellationTokenSource;
+
+        public EmptyForm()
+        {
+            InitializeComponent();
+
+            CancellationTokenSource = new();
+
+            HideWindow();
+
+            this.Disposed += EmptyForm_Disposed;
+            this.Load += EmptyForm_Load;
+
+            ConnectHandlers();
+        }
+
+        private void EmptyForm_Load(object? sender, EventArgs e)
+        {
+            AddIcon();
+        }
+
+        private void ConnectHandlers()
+        {
+            TasksHelper.Instance.TaskStateUpdated += HandleTaskStateUpdate;
+            TasksHelper.Instance.TaskListUpdated += HandleTasksListUpdate;
+        }
+
+        private void DisconnectHandlers()
+        {
+            TasksHelper.Instance.TaskStateUpdated -= HandleTaskStateUpdate;
+            TasksHelper.Instance.TaskListUpdated -= HandleTasksListUpdate;
+        }
+
+        private void HandleTaskStateUpdate(Tuple<string, bool> taskStateUpdate)
+        {
+            if (taskStateUpdate.Item2)
+            {
+                if (IsAnyTaskHasState(!taskStateUpdate.Item2))
+                {
+                    UpdateIcon("trayLogoStartedNotAll", GetNowRunnedComponentsString());
+                }
+                else
+                {
+                    UpdateIcon("trayLogoStarted", LocaleHelper.GetLocaleString("StartedAll"));
+                }
+            }
+            else
+            {
+                if (IsAnyTaskHasState(!taskStateUpdate.Item2))
+                {
+                    UpdateIcon("trayLogoStartedNotAll", GetNowRunnedComponentsString());
+                }
+                else
+                {
+                    UpdateIcon("trayLogoStopped", LocaleHelper.GetLocaleString("AllStopped"));
+                }
+            }
+        }
+
+        private void HandleTasksListUpdate()
+        {
+
+        }
+
+        private static bool IsAnyTaskHasState(bool state)
+        {
+            foreach (var task in TasksHelper.Instance.Tasks)
+            {
+                if (task.ProcessManager != null)
+                {
+                    if (task.ProcessManager.GetState() == state) return true;
+                }
+            }
+            return false;
+        }
+
+        private static string GetNowRunnedComponentsString()
+        {
+            string result = string.Empty;
+            int cnt = 0;
+            foreach (var task in TasksHelper.Instance.Tasks)
+            {
+                if (task.ProcessManager.GetState())
+                {
+                    result += $"{LocaleHelper.GetLocalizedComponentName(task.Id)}, ";
+                    cnt++;
+                }
+            }
+            result = result[..^2];
+            return string.Format(cnt > 1 ? LocaleHelper.GetLocaleString("StartedNowS") : LocaleHelper.GetLocaleString("StartedNow"), result);
+        }
+
+        private void HideWindow()
+        {
+            this.ShowInTaskbar = false;
+            this.Visible = false;
+
+            StartPosition = FormStartPosition.Manual;
+
+            Location = new Point(-2000, -2000);
+            Size = new Size(1, 1);
+        }
+
+        private void ShowContextMenuAt(Point location)
+        {
+            TrayMenuForm = new();
+            TrayMenuForm.ShowWindow(location);
+            TrayMenuForm.Hided += HideContextMenu;
+        }
+
+        private void HideContextMenu()
+        {
+            if (TrayMenuForm != null)
+            {
+                TrayMenuForm.Hided -= HideContextMenu;
+                TrayMenuForm.Close();
+                TrayMenuForm.Dispose();
+
+                TrayMenuForm = null;
+            }
+        }
+
+        private static nint LoadIcon(string name)
+        {
+            var icon = Utils.GetBitmapFromResourses($"CDPIUI_TrayIcon.Assets.{name}.ico");
+            if (icon != null)
+            {
+                nint result = icon.GetHicon();
+                icon.Dispose();
+
+                return result;
+            }
+            return nint.Zero;
+        }
+
+        public async void BeginIconVisibilityCheck()
+        {
+            while (CancellationTokenSource!= null && !CancellationTokenSource.IsCancellationRequested)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(10));
+                if (!TryShowIcon())
+                {
+                    BeginIconVisibilityCheck();
+                }
+                else
+                {
+                    UpdateIcon("trayLogoNormal", string.Empty);
+                    CancellationTokenSource.Cancel();
+                }
+            }
+            
+        }
+
+        public bool TryShowIcon()
+        {
+            RECT rect = GetRectIcon();
+            if (rect.top == 0 && rect.left == 0 && rect.right == 0 && rect.bottom == 0)
+            {
+                DeleteIcon();
+                AddIcon(showIcon:false);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private static void UpdateIcon(string iconName, string toolTip)
+        {
+            if (string.IsNullOrEmpty(toolTip)) toolTip = "CDPI UI";
+            else toolTip = $"CDPI UI\n{toolTip}";
+            if (string.IsNullOrEmpty(iconName)) return;
+
+            NOTIFYICONDATA data = new();
+
+            data.cbSize = Marshal.SizeOf(data);
+            data.guidItem = IconDisplayGuid;
+            data.uCallbackMessage = WM_MYMESSAGE;
+            data.hIcon = LoadIcon(iconName);
+            data.uFlags = NotifyFlags.NIF_ICON | NotifyFlags.NIF_GUID | NotifyFlags.NIF_MESSAGE | NotifyFlags.NIF_TIP |
+                          NotifyFlags.NIF_SHOWTIP;
+            data.szTip = toolTip;
+
+            var result = Shell_NotifyIcon(NotifyCommand.NIM_MODIFY, ref data);
+
+            Debug.WriteLine($"RESULT {result}");
+        }
+
+        public void AddIcon(bool showIcon=true)
+        {
+            NOTIFYICONDATA data = new();
+
+            data.cbSize = Marshal.SizeOf(data);
+            data.hWnd = this.Handle;
+            data.guidItem = IconDisplayGuid;
+            data.uCallbackMessage = WM_MYMESSAGE;
+            if (showIcon) data.hIcon = LoadIcon("trayLogoNormal");
+            data.szTip = "CDPI UI";
+
+            data.uFlags = NotifyFlags.NIF_ICON | NotifyFlags.NIF_GUID | NotifyFlags.NIF_MESSAGE | NotifyFlags.NIF_TIP |
+                          NotifyFlags.NIF_SHOWTIP;
+
+            Shell_NotifyIcon(NotifyCommand.NIM_ADD, ref data);
+
+            data.uVersion = NOTIFYICON_VERSION_4;
+            Shell_NotifyIcon(NotifyCommand.NIM_SETVERSION, ref data);
+        }
+
+        private static void DeleteIcon()
+        {
+            NOTIFYICONDATA data = new NOTIFYICONDATA();
+            data.cbSize = Marshal.SizeOf(data);
+            data.uFlags = NotifyFlags.NIF_GUID;
+            data.guidItem = IconDisplayGuid;
+
+            Shell_NotifyIcon(NotifyCommand.NIM_DELETE, ref data);
+        }
+
+        private static RECT GetRectIcon()
+        {
+            NOTIFYICONIDENTIFIER notifyIcon = new NOTIFYICONIDENTIFIER();
+
+            notifyIcon.cbSize = Marshal.SizeOf(notifyIcon);
+
+            notifyIcon.guidItem = IconDisplayGuid;
+            int hresult = Shell_NotifyIconGetRect(ref notifyIcon, out RECT rect);
+
+            return rect;
+        }
+
+        private static async void MaximizeApp()
+        {
+            if (!await PipeServer.Instance.SendMessage("WINDOW:SHOW_MAIN"))
+            {
+                RunHelper.RunAsDesktopUser(Path.Combine(Utils.GetDataDirectory(), "CDPIUI.exe"), string.Empty);
+            }
+        }
+
+        private void EmptyForm_Disposed(object? sender, EventArgs e)
+        {
+            CancellationTokenSource?.Cancel();
+            DeleteIcon();
+            DisconnectHandlers();
+            this.Disposed -= EmptyForm_Disposed;
+        }
+
+
+        #region MessageHandler
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_MYMESSAGE)
+            {
+                //(Int32)m.LParam & 0x0000FFFF get the low 2 bytes of LParam, we dont need the high ones. 
+                //(Int32)m.WParam & 0x0000FFFF is the X coordinate and 
+                //((Int32)m.WParam & 0xFFFF0000) >> 16 the Y
+                switch ((Int32)m.LParam & 0x0000FFFF)
+                {
+                    case NIN_BALLOONHIDE:
+
+                        break;
+                    case NIN_BALLOONSHOW:
+
+                        break;
+                    case NIN_BALLOONTIMEOUT:
+
+                        break;
+                    case NIN_BALLOONUSERCLICK:
+                        //user clicked on balloon
+
+                        break;
+                    case NIN_SELECT:
+
+                        break;
+                    case WM_CONTEXTMENU:
+                        var rect = GetRectIcon();
+                        ShowContextMenuAt(new Point(rect.left, rect.top));
+                        break;
+
+                    //get what mouse messages you want
+                    case WM_LBUTTONDOWN:
+                        MaximizeApp();
+                        break;
+                    default:
+
+                        break;
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+
+        #endregion
+
+        #region WinAPI
+
+        public const Int32 WM_MYMESSAGE = 0x8000; //WM_APP
+        public const Int32 NOTIFYICON_VERSION_4 = 0x4;
+
+        //messages
+        public const Int32 WM_CONTEXTMENU = 0x7B;
+        public const Int32 NIN_BALLOONHIDE = 0x403;
+        public const Int32 NIN_BALLOONSHOW = 0x402;
+        public const Int32 NIN_BALLOONTIMEOUT = 0x404;
+        public const Int32 NIN_BALLOONUSERCLICK = 0x405;
+        public const Int32 NIN_KEYSELECT = 0x403;
+        public const Int32 NIN_SELECT = 0x400;
+        public const Int32 NIN_POPUPOPEN = 0x406;
+        public const Int32 NIN_POPUPCLOSE = 0x407;
+        public const Int32 WM_LBUTTONDOWN = 0x0201;
+
+        public const Int32 NIIF_USER = 0x4;
+        public const Int32 NIIF_NONE = 0x0;
+        public const Int32 NIIF_INFO = 0x1;
+        public const Int32 NIIF_WARNING = 0x2;
+        public const Int32 NIIF_ERROR = 0x3;
+        public const Int32 NIIF_LARGE_ICON = 0x20;
+
+
+
+        public enum NotifyFlags
+        {
+            NIF_MESSAGE = 0x01,
+            NIF_ICON = 0x02,
+            NIF_TIP = 0x04,
+            NIF_INFO = 0x10,
+            NIF_STATE = 0x08,
+            NIF_GUID = 0x20,
+            NIF_SHOWTIP = 0x80
+        }
+
+        public enum NotifyCommand { NIM_ADD = 0x0, NIM_DELETE = 0x2, NIM_MODIFY = 0x1, NIM_SETVERSION = 0x4 }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct NOTIFYICONDATA
+        {
+            public Int32 cbSize;
+            public IntPtr hWnd;
+            public Int32 uID;
+            public NotifyFlags uFlags;
+            public Int32 uCallbackMessage;
+            public IntPtr hIcon;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+            public String szTip;
+            public Int32 dwState;
+            public Int32 dwStateMask;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            public String szInfo;
+            public Int32 uVersion;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public String szInfoTitle;
+            public Int32 dwInfoFlags;
+            public Guid guidItem; //> IE 6
+            public IntPtr hBalloonIcon;
+        }
+
+        [DllImport("shell32.dll")]
+        public static extern System.Int32 Shell_NotifyIcon(NotifyCommand cmd, ref NOTIFYICONDATA data);
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public Int32 left;
+            public Int32 top;
+            public Int32 right;
+            public Int32 bottom;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct NOTIFYICONIDENTIFIER
+        {
+            public Int32 cbSize;
+            public IntPtr hWnd;
+            public Int32 uID;
+            public Guid guidItem;
+        }
+
+        //Works with Shell32.dll (version 6.1 or later)
+        [DllImport("shell32.dll", SetLastError = true)]
+        public static extern int Shell_NotifyIconGetRect([In] ref NOTIFYICONIDENTIFIER identifier, [Out] out RECT iconLocation);
+
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // width of ellipse
+            int nHeightEllipse // height of ellipse
+        );
+
+        #endregion
+    }
+}
---
+++ CDPIUI_TrayIcon/Forms/TrayMenuForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿using CDPIUI_TrayIcon.Helper;
+using System;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+
+namespace CDPIUI_TrayIcon.Forms
+{
+    partial class TrayMenuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+        
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ExitButton = new CDPIUI_TrayIcon.Controls.ModernButtonUserControl();
+            MaximizeButton = new CDPIUI_TrayIcon.Controls.ModernButtonUserControl();
+            Separator = new CDPIUI_TrayIcon.Controls.ModernSeparatorUserControl();
+            MainPanel = new CDPIUI_TrayIcon.Controls.BackgroundUserControl();
+            BasicActionsPanel = new Panel();
+            ComponentPanel = new Panel();
+            MainPanel.SuspendLayout();
+            BasicActionsPanel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // ExitButton
+            // 
+            ExitButton.BackColor = Color.Transparent;
+            ExitButton.Bold = false;
+            ExitButton.DisplayImage = null;
+            ExitButton.DisplayText = "Exit";
+            ExitButton.ForeColor = Color.White;
+            ExitButton.Glyph = "";
+            ExitButton.Location = new Point(0, 36);
+            ExitButton.Margin = new Padding(0, 2, 0, 2);
+            ExitButton.Name = "ExitButton";
+            ExitButton.Size = new Size(275, 32);
+            ExitButton.TabIndex = 0;
+            // 
+            // MaximizeButton
+            // 
+            MaximizeButton.BackColor = Color.Transparent;
+            MaximizeButton.Bold = true;
+            MaximizeButton.DisplayImage = null;
+            MaximizeButton.DisplayText = "Maximize";
+            MaximizeButton.ForeColor = Color.White;
+            MaximizeButton.Glyph = "";
+            MaximizeButton.Location = new Point(0, 2);
+            MaximizeButton.Margin = new Padding(0, 2, 0, 2);
+            MaximizeButton.Name = "MaximizeButton";
+            MaximizeButton.Size = new Size(275, 32);
+            MaximizeButton.TabIndex = 1;
+            // 
+            // Separator
+            // 
+            Separator.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            Separator.BackColor = Color.FromArgb(53, 53, 53);
+            Separator.Location = new Point(0, 368);
+            Separator.Margin = new Padding(0, 2, 0, 2);
+            Separator.Name = "Separator";
+            Separator.Size = new Size(283, 1);
+            Separator.TabIndex = 2;
+            // 
+            // MainPanel
+            // 
+            MainPanel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            MainPanel.BackColor = Color.FromArgb(44, 44, 44);
+            MainPanel.BackgroundColor = Color.Transparent;
+            MainPanel.BorderColor = Color.FromArgb(0, 0, 0, 0);
+            MainPanel.BorderWidth = 1F;
+            MainPanel.Controls.Add(BasicActionsPanel);
+            MainPanel.Controls.Add(Separator);
+            MainPanel.Controls.Add(ComponentPanel);
+            MainPanel.Location = new Point(1, 1);
+            MainPanel.Margin = new Padding(0);
+            MainPanel.Name = "MainPanel";
+            MainPanel.Padding = new Padding(4, 2, 4, 2);
+            MainPanel.Radius = 0;
+            MainPanel.Size = new Size(283, 442);
+            MainPanel.TabIndex = 3;
+            // 
+            // BasicActionsPanel
+            // 
+            BasicActionsPanel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            BasicActionsPanel.Controls.Add(MaximizeButton);
+            BasicActionsPanel.Controls.Add(ExitButton);
+            BasicActionsPanel.Location = new Point(4, 371);
+            BasicActionsPanel.Margin = new Padding(4, 0, 4, 2);
+            BasicActionsPanel.Name = "BasicActionsPanel";
+            BasicActionsPanel.Size = new Size(275, 73);
+            BasicActionsPanel.TabIndex = 5;
+            // 
+            // ComponentPanel
+            // 
+            ComponentPanel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            ComponentPanel.BackColor = Color.Transparent;
+            ComponentPanel.Location = new Point(4, 4);
+            ComponentPanel.Margin = new Padding(0);
+            ComponentPanel.Name = "ComponentPanel";
+            ComponentPanel.Padding = new Padding(4, 2, 4, 0);
+            ComponentPanel.Size = new Size(279, 362);
+            ComponentPanel.TabIndex = 4;
+            // 
+            // TrayMenuForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DimGray;
+            ClientSize = new Size(285, 444);
+            ControlBox = false;
+            Controls.Add(MainPanel);
+            FormBorderStyle = FormBorderStyle.None;
+            MaximizeBox = false;
+            MdiChildrenMinimizedAnchorBottom = false;
+            MinimizeBox = false;
+            Name = "TrayMenuForm";
+            Padding = new Padding(1);
+            ShowIcon = false;
+            ShowInTaskbar = false;
+            SizeGripStyle = SizeGripStyle.Hide;
+            Text = "TrayMenuForm";
+            TopMost = true;
+            MainPanel.ResumeLayout(false);
+            BasicActionsPanel.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Controls.ModernListView MainFlowLayoutPanel;
+        private Controls.ModernButtonUserControl ExitButton;
+        private Controls.ModernButtonUserControl MaximizeButton;
+        private Controls.ModernSeparatorUserControl Separator;
+        private Controls.BackgroundUserControl MainPanel;
+        private Panel ComponentPanel;
+        private Panel BasicActionsPanel;
+    }
+}
