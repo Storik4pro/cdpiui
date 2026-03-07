58fe98d58315cf37f0ddf009431883a37ce77a7c
Storik4pro <serstor15@gmail.com>
2026-03-07 19:37:02 +0300

    Add new controls to ModernTrayMenu

---
+++ CDPIUI_TrayIcon/Controls/IconButtonUserControl.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace CDPIUI_TrayIcon.Controls
+{
+    partial class IconButtonUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            IconGlyph = new Label();
+            SuspendLayout();
+            // 
+            // IconGlyph
+            // 
+            IconGlyph.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            IconGlyph.BackColor = Color.Transparent;
+            IconGlyph.Font = new Font("Segoe Fluent Icons", 16F, FontStyle.Regular, GraphicsUnit.Pixel, 0);
+            IconGlyph.ForeColor = Color.White;
+            IconGlyph.Location = new Point(0, 0);
+            IconGlyph.Margin = new Padding(0);
+            IconGlyph.Name = "IconGlyph";
+            IconGlyph.Size = new Size(30, 30);
+            IconGlyph.TabIndex = 4;
+            IconGlyph.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // IconButtonUserControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Transparent;
+            Controls.Add(IconGlyph);
+            Name = "IconButtonUserControl";
+            Size = new Size(30, 30);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label IconGlyph;
+    }
+}
---
+++ CDPIUI_TrayIcon/Controls/ModernButtonUserControl.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Windows.UI.Core;
+
+namespace CDPIUI_TrayIcon.Controls
+{
+    public partial class ModernButtonUserControl : UserControl
+    {
+        private string _text = string.Empty;
+        public string DisplayText
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                _text = value;
+                MainTextBox.Text = value;
+            }
+        }
+
+        private bool _bold = false;
+        public bool Bold
+        {
+            get { return _bold; }
+            set
+            {
+                _bold = value;
+                MainTextBox.Font = new Font(MainTextBox.Font, _bold? FontStyle.Bold : FontStyle.Regular);
+            }
+        }
+
+        private string _glyph = string.Empty;
+        public string Glyph
+        {
+            get
+            {
+                return _glyph;
+            }
+            set
+            {
+                _glyph = value;
+                IconGlyph.Text = value;
+                ChangeVisibility();
+            }
+        }
+
+        private Image? _image;
+        public Image? DisplayImage
+        {
+            get
+            {
+                return _image;
+            }
+            set
+            {
+                _image = value;
+                MainPictureBox.Image = value;
+                ChangeVisibility();
+            }
+        }
+
+        public EventHandler? Clicked;
+
+
+        public ModernButtonUserControl()
+        {
+            InitializeComponent();
+
+            this.BackColor = Color.FromArgb(0, 45, 45, 45);
+            this.ForeColor = Color.White;
+
+            ConnectHandlers();
+        }
+
+        #region HandlersManagement
+
+        private void ConnectHandlers()
+        {
+            this.MouseEnter += ModernButtonUserControl_MouseEnter;
+            this.MouseLeave += ModernButtonUserControl_MouseLeave;
+
+            this.MouseClick += ModernButtonUserControl_MouseClick;
+
+
+            foreach (Control control in Controls)
+            {
+                control.MouseEnter += new EventHandler(this.ModernButtonUserControl_MouseEnter);
+                control.MouseLeave += new EventHandler(this.ModernButtonUserControl_MouseLeave);
+
+                control.MouseClick += this.ModernButtonUserControl_MouseClick;
+            }
+
+            this.Disposed += ModernButtonUserControl_Disposed; ;
+        }
+
+
+        private void DisconnectHandlers()
+        {
+            this.MouseEnter -= ModernButtonUserControl_MouseEnter;
+            this.MouseLeave -= ModernButtonUserControl_MouseLeave;
+
+            this.MouseClick -= ModernButtonUserControl_MouseClick;
+
+
+            foreach (Control control in Controls)
+            {
+                control.MouseEnter -= new EventHandler(this.ModernButtonUserControl_MouseEnter);
+                control.MouseLeave -= new EventHandler(this.ModernButtonUserControl_MouseLeave);
+
+                control.MouseClick -= this.ModernButtonUserControl_MouseClick;
+            }
+
+            this.Disposed -= ModernButtonUserControl_Disposed;
+        }
+
+        #endregion
+
+        private void ModernButtonUserControl_MouseClick(object? sender, MouseEventArgs e)
+        {
+            Clicked?.Invoke(this, e);
+        }
+
+        private void ChangeVisibility()
+        {
+            if (string.IsNullOrEmpty(IconGlyph.Text))
+            {
+                IconGlyph.Visible = false;
+                MainPictureBox.Visible = true;
+            }
+            else
+            {
+                IconGlyph.Visible = true;
+                MainPictureBox.Visible = false;
+            }
+        }
+
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            base.OnMouseClick(e);
+        }
+
+        private void ModernButtonUserControl_MouseLeave(object? sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(0, 45, 45, 45);
+        }
+
+        private void ModernButtonUserControl_MouseEnter(object? sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(50, 50, 50);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainTextBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void ModernButtonUserControl_Disposed(object? sender, EventArgs e)
+        {
+            DisconnectHandlers();
+        }
+    }
+}
+
