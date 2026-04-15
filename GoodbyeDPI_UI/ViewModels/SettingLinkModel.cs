6999109e3df3751574aebbe1ed54d2cc58f41648
Storik4pro <serstor15@gmail.com>
2026-04-15 21:10:40 +0300

    Add new models

---
+++ GoodbyeDPI_UI/ViewModels/SettingLinkModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CDPI_UI.ViewModels
+{
+    public enum LinkedActions
+    {
+        CreateNewConfigForComponent,
+        EditCurrentConfig,
+
+        // TgWsProxy only
+        OpenProxyInTelegram,
+
+    }
+
+    public class SettingLinkModel
+    {
+        public string DisplayName { get; set; }
+        public LinkedActions Action {  get; set; }
+    }
+}
