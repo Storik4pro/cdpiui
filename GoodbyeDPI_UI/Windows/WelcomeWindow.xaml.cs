cd77931fba592ecea3b2804175098138ef58159d
Storik4pro <serstor15@gmail.com>
2026-06-30 10:44:52 +0300

    Some work for welcome window

---
+++ GoodbyeDPI_UI/Windows/WelcomeWindow.xaml.cs
@@ -0,0 +1,40 @@
+using CDPI_UI.Default;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using WinUI3Localizer;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace CDPI_UI
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class WelcomeWindow : TemplateWindow
+    {
+        private ILocalizer localizer = Localizer.Get();
+        public WelcomeWindow()
+        {
+            InitializeComponent();
+
+            WindowTitle = localizer.GetLocalizedString("WelcomeWindowTitle");
+            IconUri = @"Assets/Icons/find_error.png";
+            this.CustomTitleBarUserControl = TitleBarUserControl;
+
+            
+        }
+    }
+}
