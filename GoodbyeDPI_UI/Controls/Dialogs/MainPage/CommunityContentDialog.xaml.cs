d5c4d9e1991e10f17e88d12d3d138d1f59393534
Storik4pro <serstor15@gmail.com>
2026-07-15 10:28:28 +0300

    Add new dialog

---
+++ GoodbyeDPI_UI/Controls/Dialogs/MainPage/CommunityContentDialog.xaml.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+using CDPI_UI.ViewModels;
+using CDPI_UI.Helper;
+using WinUI3Localizer;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace CDPI_UI.Controls.Dialogs.MainPage;
+
+public sealed partial class CommunityContentDialog : ContentDialog
+{
+    private ObservableCollection<UrlViewModel> Models = [];
+
+    private ILocalizer localizer = Localizer.Get();
+
+    public CommunityContentDialog()
+    {
+        InitializeComponent();
+
+        MainListView.ItemsSource = Models;
+
+        Init();
+    }
+
+    private UrlViewModel CreateNewModel(string name, string description, string url)
+    {
+        string locName = localizer.GetLocalizedString(name);
+        string locDesc = localizer.GetLocalizedString(description);
+        return new()
+        {
+            Name = locName,
+            Description = locDesc,
+            Url = url
+        };
+    }
+
+    private void Init()
+    {
+        Models.Clear();
+        Models.Add(CreateNewModel("TelegramMainUrlName", "TelegramMainUrlDescription", UrlOpenHelper.TelegramMainUrl));
+        Models.Add(CreateNewModel("NTCPartyName", "NTCPartyDescription", UrlOpenHelper.NTCParty));
+        Models.Add(CreateNewModel("BBDName", "BBDDecription", UrlOpenHelper.BBD));
+
+        Models.Add(CreateNewModel("TelegramMemeUrlName", "TelegramMemeUrlDescription", UrlOpenHelper.TelegramMemeUrl));
+        Models.Add(CreateNewModel("TelegramLUrlName", "TelegramLUrlDescription", UrlOpenHelper.TelegramLUrl));
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        var data = (sender as FrameworkElement).DataContext as UrlViewModel;
+
+        UrlOpenHelper.LaunchUrl(data.Url);
+    }
+}
