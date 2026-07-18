b9d95e6d5d8d22ae98e54277cc354025e8e68750
Storik4pro <serstor15@gmail.com>
2026-07-18 13:25:15 +0300

    Rename CDPIUI_TrayIcon to CDPIUI.TrayIcon

--- CDPIUI.TrayIcon/Properties/Resources.Designer.cs
+++ CDPIUI.TrayIcon/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CDPIUI_TrayIcon.Properties {
+namespace CDPIUI.TrayIcon.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace CDPIUI_TrayIcon.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CDPIUI_TrayIcon.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CDPIUI.TrayIcon.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
