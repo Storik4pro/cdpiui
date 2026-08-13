63b025aa3f63ebc8e96c280764ed6b75869457cb
Storik4pro <serstor15@gmail.com>
2026-08-13 15:09:33 +0300

    Move some settings to view models

--- CDPIUI/ViewModels/MainPageMarkupViewModel.cs
+++ CDPIUI/ViewModels/MainPageMarkupViewModel.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.UI.Xaml.Media;
+﻿using CDPIUI.Core;
+using CDPIUI.Helper;
+using CDPIUI.Helper.UserExperience;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WinUI3Localizer;
 
 namespace CDPIUI.ViewModels
 {
@@ -15,9 +23,88 @@ namespace CDPIUI.ViewModels
     public class MainPageMarkupViewModel
     {
         public string Name { get; set; }
+
+        public string Description { get; set; }
         
         public MarkupTypes Type { get; set; }
 
         public ImageSource ImageSource { get; set; }
+    }
+
+    public partial class MainPageMarkupManager : INotifyPropertyChanged
+    {
+        public static MainPageMarkupManager Instance { get; } = new();
+
+        public readonly ObservableCollection<MainPageMarkupViewModel> Markups = [];
+
+        public bool IsModernMarkupSelected { get => CurrentMarkup.Type == MarkupTypes.Modern; }
+
+        public MainPageMarkupManager()
+        {
+            LoadMarkups();
+
+            CurrentMarkup = Markups.FirstOrDefault(x => x.Type.ToString() == SettingsManager.Instance.GetValue<string>("APPEARANCE", "mainPageMarkup"));
+        }
+
+        private MainPageMarkupViewModel currentMarkup;
+        public MainPageMarkupViewModel CurrentMarkup
+        {
+            get => currentMarkup;
+            private set
+            {
+                if (ReferenceEquals(currentMarkup, value))
+                    return;
+
+                currentMarkup = value;
+                PropertyChanged?.Invoke(
+                    this,
+                    new PropertyChangedEventArgs(nameof(CurrentMarkup)));
+                PropertyChanged?.Invoke(
+                    this,
+                    new PropertyChangedEventArgs(nameof(IsModernMarkupSelected)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void LoadMarkups()
+        {
+            ILocalizer localizer = Localizer.Get();
+
+            Markups.Clear();
+            Markups.Add(new()
+            {
+                Type = MarkupTypes.Classic,
+                Name = localizer.GetLocalizedString("ClassicMarkup"),
+                Description = localizer.GetLocalizedString("ClassicMarkupDescription"),
+                ImageSource = new BitmapImage(UIHelper.GetUriFromString("ms-appx:///Assets/Preview/MainPageMarkup/ClassicMarkup.png"))
+            });
+            Markups.Add(new()
+            {
+                Type = MarkupTypes.Modern,
+                Name = localizer.GetLocalizedString("ModernMarkup"),
+                Description = localizer.GetLocalizedString("ModernMarkupDescription"),
+                ImageSource = new BitmapImage(UIHelper.GetUriFromString("ms-appx:///Assets/Preview/MainPageMarkup/ModernMarkup.png"))
+            });
+        }
+
+        public void SaveMarkupSettings(MainPageMarkupViewModel model)
+        {
+            if (model == CurrentMarkup)
+            {
+                return; 
+            }
+
+            CurrentMarkup = model;
+            SettingsManager.Instance.SetValue<string>("APPEARANCE", "mainPageMarkup", model.Type.ToString());
+        }
+
+        public void HandleCollectionChanged(
+            object sender,
+            SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.FirstOrDefault() is MainPageMarkupViewModel model)
+                SaveMarkupSettings(model);
+        }
     }
 }
--- CDPIUI/ViewModels/ThemeViewModel.cs
+++ CDPIUI/ViewModels/ThemeViewModel.cs
@@ -1,11 +1,18 @@
-﻿using CDPIUI.Helper;
+﻿using CDPIUI.Core;
+using CDPIUI.Helper;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using WinUI3Localizer;
+using Application = Microsoft.UI.Xaml.Application;
 
 namespace CDPIUI.ViewModels
 {
@@ -25,5 +32,96 @@ namespace CDPIUI.ViewModels
         public Brush SecondBackgrounBrush { get => UIHelper.HexToSolidColorBrushConverter(SecondBackgroundColorHEX); }
 
         public ImageSource ImageSource;
+    }
+
+    public class ApplicationThemeManager : INotifyPropertyChanged
+    {
+        public static ApplicationThemeManager Instance { get; } = new();
+
+        public readonly ObservableCollection<ThemeViewModel> Themes = [];
+
+        public ApplicationThemeManager()
+        {
+            LoadThemes();
+
+            ElementTheme theme = ((App)Application.Current).GetCurrentTheme();
+            CurrentTheme = Themes.FirstOrDefault(x => x.FriendlyThemeId == theme);
+        }
+
+        private ThemeViewModel currentTheme;
+        public ThemeViewModel CurrentTheme
+        {
+            get => currentTheme;
+            private set
+            {
+                if (ReferenceEquals(currentTheme, value))
+                    return;
+
+                currentTheme = value;
+                PropertyChanged?.Invoke(
+                    this,
+                    new PropertyChangedEventArgs(nameof(CurrentTheme)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void LoadThemes()
+        {
+            ILocalizer localizer = Localizer.Get();
+
+            Themes.Clear();
+            Themes.Add(new()
+            {
+                Guid = new Guid(),
+                FriendlyThemeId = ElementTheme.Dark,
+                Name = localizer.GetLocalizedString("DarkTheme"),
+                FirstBackgroundColorHEX = "#000000",
+                SecondBackgroundColorHEX = "#000000",
+            });
+            Themes.Add(new()
+            {
+                Guid = new Guid(),
+                FriendlyThemeId = ElementTheme.Light,
+                Name = localizer.GetLocalizedString("LightTheme"),
+                FirstBackgroundColorHEX = "#F3F3F3",
+                SecondBackgroundColorHEX = "#F3F3F3",
+            });
+            Themes.Add(new()
+            {
+                Guid = new Guid(),
+                FriendlyThemeId = ElementTheme.Default,
+                Name = localizer.GetLocalizedString("SystemTheme"),
+                FirstBackgroundColorHEX = "#000000",
+                SecondBackgroundColorHEX = "#F3F3F3",
+            });
+        }
+
+        public void SaveThemeSettings(ThemeViewModel model)
+        {
+            if (model == CurrentTheme)
+            {
+                return;
+            }
+
+            ElementTheme newTheme = ElementTheme.Default;
+
+            newTheme = model.FriendlyThemeId;
+
+            CurrentTheme = model;
+            SettingsManager.Instance.SetValue<string>("APPEARANCE", "Theme", newTheme.ToString());
+
+            ((App)Application.Current).UpdateThemeForAllWindows(newTheme);
+
+            
+        }
+
+        public void HandleCollectionThemeChanged(
+            object sender,
+            SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.FirstOrDefault() is ThemeViewModel model)
+                SaveThemeSettings(model);
+        }
     }
 }
