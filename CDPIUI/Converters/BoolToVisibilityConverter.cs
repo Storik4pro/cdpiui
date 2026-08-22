d2154f183f1361eea4bb3e499090971165c68999
Storik4pro <serstor15@gmail.com>
2026-08-22 12:40:09 +0300

    Add reversed boolToVisibility converter

--- CDPIUI/Converters/BoolToVisibilityConverter.cs
+++ CDPIUI/Converters/BoolToVisibilityConverter.cs
@@ -22,4 +22,19 @@ namespace CDPIUI.Converters
             throw new NotSupportedException();
         }
     }
+
+    public class ReversedBoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool b && b)
+                return Visibility.Collapsed;
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+    }
 }
