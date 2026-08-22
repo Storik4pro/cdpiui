792765d89f19bbc37d0a34f102500d67fe41464c
Storik4pro <serstor15@gmail.com>
2026-08-22 12:49:27 +0300

    Change READ to read one byte per operation

--- CDPIUI.Shared/Pipe/StreamString.cs
+++ CDPIUI.Shared/Pipe/StreamString.cs
@@ -16,14 +16,31 @@ namespace CDPIUI.Shared.Pipe
         public async Task<string> ReadStringAsync(CancellationToken token = default)
         {
             byte[] lenBuffer = new byte[2];
-            await ioStream.ReadAsync(lenBuffer, 0, 2, token).ConfigureAwait(false);
+            await ReadExactlyAsync(lenBuffer, token).ConfigureAwait(false);
 
             int len = lenBuffer[0] * 256 + lenBuffer[1];
 
             byte[] inBuffer = new byte[len];
-            await ioStream.ReadAsync(inBuffer, 0, len, token).ConfigureAwait(false);
+            await ReadExactlyAsync(inBuffer, token).ConfigureAwait(false);
 
             return streamEncoding.GetString(inBuffer);
+        }
+
+        private async Task ReadExactlyAsync(byte[] buffer, CancellationToken token)
+        {
+            int offset = 0;
+            while (offset < buffer.Length)
+            {
+                int bytesRead = await ioStream
+                    .ReadAsync(buffer, offset, buffer.Length - offset, token)
+                    .ConfigureAwait(false);
+                if (bytesRead == 0)
+                {
+                    throw new EndOfStreamException();
+                }
+
+                offset += bytesRead;
+            }
         }
 
         public async Task<int> WriteStringAsync(string outString, CancellationToken token = default)
