144ba95dd4179c021d85f862548353bc207399e7
Storik4pro <serstor15@gmail.com>
2026-09-05 14:12:13 +0300

    Add new converter

--- CDPIUI/Converters/ObjectToBoolConverter.cs
+++ CDPIUI/Converters/ObjectToBoolConverter.cs
@@ -22,4 +22,18 @@ namespace CDPIUI.Converters
             throw new NotSupportedException();
         }
     }
+    public class ObjectToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is null)
+                return Visibility.Collapsed;
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+    }
 }
