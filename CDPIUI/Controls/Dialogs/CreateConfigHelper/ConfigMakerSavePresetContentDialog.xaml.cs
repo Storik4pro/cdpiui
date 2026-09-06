99f88ef18886d561ff142c96da1f79e43bda8300
Storik4pro <serstor15@gmail.com>
2026-09-06 13:55:32 +0300

    Add "overwrite" option

--- CDPIUI/Controls/Dialogs/CreateConfigHelper/ConfigMakerSavePresetContentDialog.xaml.cs
+++ CDPIUI/Controls/Dialogs/CreateConfigHelper/ConfigMakerSavePresetContentDialog.xaml.cs
@@ -1,12 +1,24 @@
 #nullable enable
 
 using Microsoft.UI.Xaml.Controls;
+using MS.WindowsAPICodePack.Internal;
 using WinUI3Localizer;
 
 namespace CDPIUI.Controls.Dialogs.CreateConfigHelper;
 
+public enum ConfigMakerSavePresetContentDialogResult
+{
+    SaveAsNew,
+    Overwrite,
+    Cancel
+}
+
 public sealed partial class ConfigMakerSavePresetContentDialog : ContentDialog
 {
+    public ConfigMakerSavePresetContentDialogResult Result = ConfigMakerSavePresetContentDialogResult.Cancel;
+
+    public bool OverwriteEnable { get; set; } = true;
+
     public ConfigMakerSavePresetContentDialog(string suggestedName = "")
     {
         InitializeComponent();
@@ -24,14 +36,22 @@ public sealed partial class ConfigMakerSavePresetContentDialog : ContentDialog
     private void PresetNameTextBox_TextChanged(object sender, TextChangedEventArgs e) =>
         UpdatePrimaryButton();
 
-    private void UpdatePrimaryButton() =>
-        IsPrimaryButtonEnabled = !string.IsNullOrWhiteSpace(PresetNameTextBox.Text);
+    private void SelectorBar_SelectionChanged(object sender, Navigation.AnimatedSelectorBarSelectionChangedEventArgs e) =>
+        UpdatePrimaryButton();
+
+    private void UpdatePrimaryButton()
+    {
+        IsPrimaryButtonEnabled = SelectorBar.SelectedItem == SaveAsNewItem ? !string.IsNullOrWhiteSpace(PresetNameTextBox.Text) : true;
+    }
 
     private void ContentDialog_PrimaryButtonClick(
         ContentDialog sender,
         ContentDialogButtonClickEventArgs args)
     {
+        Result = SelectorBar.SelectedItem == SaveAsNewItem ? ConfigMakerSavePresetContentDialogResult.SaveAsNew : ConfigMakerSavePresetContentDialogResult.Overwrite;
         PresetName = PresetNameTextBox.Text.Trim();
         args.Cancel = PresetName.Length == 0;
     }
+
+    
 }
