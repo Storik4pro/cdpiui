4bd86a4e95f8d903b5a9b4b7b6caf171c90be8cb
Storik4pro <serstor15@gmail.com>
2026-09-17 13:34:34 +0300

    Fix

--- CDPIUI.Shared/Extentions/DictionaryExtention.cs
+++ CDPIUI.Shared/Extentions/DictionaryExtention.cs
@@ -7,8 +7,19 @@ namespace CDPIUI.Shared.Extentions
 {
     public static class DictionaryExtention
     {
+        /// <summary>
+        /// Get dictionary key by value. <br></br> 
+        /// If value is <see cref="string"/>, 
+        /// uses <see cref="string.Equals(string, string)"/>.<br></br>
+        /// Otherwise uses default object comparier.
+        /// </summary>
         public static KeyType GetKeyByValue<KeyType, ValueType>(this Dictionary<KeyType, ValueType> dicitionary, ValueType value) where ValueType : class 
         {
+            if (value is string stval)
+            {
+                return dicitionary.FirstOrDefault(kvp => string.Equals(kvp.Value as string, value as string, StringComparison.Ordinal)).Key;
+            }
+
             return dicitionary.FirstOrDefault(kvp => kvp.Value == value).Key;
         }
 
